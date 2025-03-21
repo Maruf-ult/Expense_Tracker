dd34fac9539d71c3d9355cb36c208bb1573acc1c
maruf_hossain <marufhossain123098@gmail.com>
2025-03-21 16:07:40 +0600

    margin padding added

--- Expense-Tracker-Client-side/src/SignUp.jsx
+++ Expense-Tracker-Client-side/src/SignUp.jsx
@@ -35,7 +35,7 @@ function SignUp() {
 
   return (
     <>
-      <div className=" h-screen  bg-red-100	background-color: rgb(254 226 226); pl-14 pt-10">
+      <div className=" h-screen  bg-red-100	background-color: rgb(254 226 226); pl-20 pt-10">
         <div className=" hero hero-content flex-col ">
           <div className="text-center lg:text-left">
             <p className="py-2 text-violet-500 font-sans text-3xl">
